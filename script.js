0c12f2d32243d258ff1253fd0f7abf17b032e170
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2025-08-22 23:07:35 +0530

    Add navigation link to Box Animation and update script.js for GSAP animations

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
-// // // gsap.to("#box1", {
-// // //   x: 1500,
-// // //   rotate: 360,
-// // //   duration: 1.5,
-// // //   delay: 1,
-// // // });
-
-// // // gsap.to("#box2", {
-// // //   x: 1500,
-// // //   backgroundColor: "green",
-// // //   duration: 1.5,
-// // //   delay: 2.5,
-// // // });
-
-// // // gsap.to("#box3", {
-// // //   x: 1500,
-// // //   scale: 0.5,
-// // //   borderRadius: "50%",
-// // //   duration: 1.5,
-// // //   delay: 4,
-// // // });
-
-// // var tl = gsap.timeline()
-
-// // tl.to("#box1", {
-// //   x: 1500,
-// //   rotate: 360,
-// //   duration: 1.5,
-// //   delay: 1,
-// // });
-
-// // tl.to("#box2", {
-// //   x: 1500,
-// //   backgroundColor: "green",
-// //   duration: 1.5,
-// // });
-
-// // tl.to("#box3", {
-// //   x: 1500,
-// //   scale: 0.5,
-// //   borderRadius: "50%",
-// //   duration: 1.5,
-// // });
-
-// var tl = gsap.timeline()
-
-// tl.from("h2", {
-//   y: -20,
-//   opacity: 0,
-//   duration: 1,
-//   delay: 0.5
+// gsap.to("#box1", {
+//   x: 1500,
+//   rotate: 360,
+//   duration: 1.5,
+//   delay: 1,
 // });
 
-// tl.from("h4", {
-//   y: -20,
-//   opacity: 0,
-//   duration: 1,
-//   delay: 0.5,
-//   stagger: 0.3
+// gsap.to("#box2", {
+//   x: 1500,
+//   backgroundColor: "green",
+//   duration: 1.5,
+//   delay: 2.5,
 // });
 
-// tl.from("h1", {
-//   y: -20,
-//   opacity: 0,
-//   duration: 0.5,
-//   delay: 0.2
+// gsap.to("#box3", {
+//   x: 1500,
+//   scale: 0.5,
+//   borderRadius: "50%",
+//   duration: 1.5,
+//   delay: 4,
 // });
+
+var tl = gsap.timeline()
+
+tl.to("#box1", {
+  x: 1500,
+  rotate: 360,
+  duration: 1.5,
+  delay: 1,
+});
+
+tl.to("#box2", {
+  x: 1500,
+  backgroundColor: "green",
+  duration: 1.5,
+});
+
+tl.to("#box3", {
+  x: 1500,
+  scale: 0.5,
+  borderRadius: "50%",
+  duration: 1.5,
+});
+
+var tl = gsap.timeline()
+
+tl.from("h2", {
+  y: -20,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5
+});
+
+tl.from("h4", {
+  y: -20,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+  stagger: 0.3
+});
+
+tl.from("h1", {
+  y: -20,
+  opacity: 0,
+  duration: 0.5,
+  delay: 0.2
+});
